c1179e75d4e93866694cc40d5e00280a7bcadd35
Jason Crouse <jcrouse@drift.com>
2020-02-20 17:02:40 -0500

    Changed some return types and changed elastic to query exact matches only

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,4 @@
 use elasticsearch::{http::transport::Transport, Elasticsearch, SearchParts};
-
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 
@@ -38,42 +37,45 @@ pub struct ToBeDefined {
     pub term: String,
 }
 
-const AWS_ES_ENDPOINT: &str = "https://search-es-driftionary-kq77fbn6hvsqt3psu3htuga7gi.us-east-1.es.amazonaws.com/es-driftionary";
+const AWS_ES_ENDPOINT: &str = "https://search-es-driftionary-kq77fbn6hvsqt3psu3htuga7gi.us-east-1.es.amazonaws.com/driftionary";
 
 pub fn add_definition(definition: AddDefinition) -> Result<String, &'static str> {
     Ok(definition.term) // TODO: implement
-}
-
-pub async fn get_definition(term: String) -> Option<DefinitionsResult> {
-    match search_elastic(term).await {
-        Ok(defn) => Some(defn),
-        Err(_e) => None,
-    }
 }
 
 pub fn request_to_be_defined(tbd: ToBeDefined) -> Result<String, &'static str> {
     Ok(tbd.term) // TODO: implement
 }
 
-async fn search_elastic(term: String) -> Result<DefinitionsResult, elasticsearch::Error> {
-    let transport = Transport::single_node(AWS_ES_ENDPOINT)?;
+pub async fn get_definition(term: String) -> Option<DefinitionsResult> {
+    let hits = search_elastic(term).await;
+    if hits.is_empty() {
+        None
+    } else {
+        let first_result = hits.get(0).unwrap();
+        Some(first_result._source.clone())
+    }
+}
+
+async fn search_elastic(term: String) -> Vec<Hit> {
+    let transport = Transport::single_node(AWS_ES_ENDPOINT).unwrap();
     let client = Elasticsearch::new(transport);
     let search_response = client
         .search(SearchParts::None)
-        .body(json!({
-          "query": {
-              "match_phrase": {
-                  "term": term
-                }
-          }
-        }))
+        .body(json!(
+        {
+            "query": {
+                "term": {
+                    "term.raw": term
+                  }
+            }
+          }))
         .allow_no_indices(true)
         .send()
-        .await?;
-    let response_body = search_response.read_body::<Value>().await?;
+        .await
+        .unwrap();
+    let response_body = search_response.read_body::<Value>().await.unwrap();
     let hits = response_body["hits"].as_object().unwrap();
     let hits: Vec<Hit> = serde_json::from_value(hits["hits"].to_owned()).unwrap();
-    let first_result = hits.get(0).unwrap();
-
-    Ok(first_result._source.clone())
+    hits
 }
